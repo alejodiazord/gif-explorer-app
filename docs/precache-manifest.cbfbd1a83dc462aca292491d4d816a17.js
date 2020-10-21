9206062663f08e6e010c6f99552c23e7e11f33df
Alejandro Diaz <alejodiazord@gmail.com>
2020-10-21 18:55:05 -0500

    Docs agregado

---
+++ docs/precache-manifest.cbfbd1a83dc462aca292491d4d816a17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328396127c078e157388f0639cf9bffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d653419ab29987efea9",
+    "url": "/static/css/main.19ee729e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d653419ab29987efea9",
+    "url": "/static/js/main.445db3cf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
